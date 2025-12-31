cb7c8009f1b6733050bfe3f84ec42b108ffa77df
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2025-12-31 17:07:06 +0530

    mySql connection successfull

--- employeeList/src/main/java/com/example/employeeList/EmployeeListApplication.java
+++ employeeList/src/main/java/com/example/employeeList/EmployeeListApplication.java
@@ -1,15 +1,17 @@
 package com.example.employeeList;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(
-        exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+
+@SpringBootApplication
 public class EmployeeListApplication {
 
 	public static void main(String[] args) {
